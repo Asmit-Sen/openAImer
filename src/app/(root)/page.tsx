7fe24dafadfb8da5b165eec6da75408be365a2a1
quantumCode <quantumcode22@gmail.com>
2026-04-10 00:25:46 +0530

    feat: Big ui overhaul add gradient component and enhance chat UI with scroll behavior

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,38 @@
-import Chat from '@/components/chat/Chat';
+import Chat from "@/components/chat/Chat";
+import Grainient from "../../components/Gradient";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-24">
-        <div className='w-1/2'>
-          <Chat />
-        </div>
+    <main className="flex h-screen w-full flex-col items-center justify-center overflow-hidden">
+      <div className=" absolute w-full h-full opacity-70">
+        <Grainient
+          color1="#FF9FFC"
+          color2="#5227FF"
+          color3="#B19EEF"
+          timeSpeed={0.25}
+          colorBalance={0}
+          warpStrength={1}
+          warpFrequency={5}
+          warpSpeed={2}
+          warpAmplitude={50}
+          blendAngle={0}
+          blendSoftness={0.05}
+          rotationAmount={500}
+          noiseScale={2}
+          grainAmount={0.1}
+          grainScale={2}
+          grainAnimated={false}
+          contrast={1.5}
+          gamma={1}
+          saturation={1}
+          centerX={0}
+          centerY={0}
+          zoom={0.9}
+        />
+      </div>
+      <div className="sm:w-1/2 w-[90%] h-full">
+        <Chat />
+      </div>
     </main>
   );
 }
